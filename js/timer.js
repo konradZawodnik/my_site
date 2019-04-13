2716688b7d97e83b4df496e1246bb49b74bbed77
realmadryt123 <jd35495@gmail.com>
2019-04-13 18:57:04 +0200

    Poprawki na stronie

--- js/timer.js
+++ js/timer.js
@@ -1,19 +1,43 @@
-function startTime() {
-    var today = new Date();
-    today.toISOString()
-    let year = today.getFullYear();
-    let month = today.getMonth() + 1;;
-    let day = today.getDate();;
-    let hour = today.getHours();
-    let minute = today.getMinutes();
-    let second = today.getSeconds();
-    minute = checkTime(minute);
-    second = checkTime(second);
-    document.getElementById('clock').innerHTML =
-      `Dzisiaj jest ${day}.${month}.${year}, godzina ${hour}: ${minute}:${second}.`;
-    var t = setTimeout(startTime, 500);
+const startTime = () => {
+  let today = new Date();
+  today.toISOString()
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;;
+  let day = today.getDate();;
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let second = today.getSeconds();
+  minute = checkTime(minute);
+  second = checkTime(second);
+  let nameOfDay;
+  switch (today.getDay()) {
+    case 0:
+      nameOfDay = "Niedziela";
+      break;
+    case 1:
+      nameOfDay = "Poniedziałek";
+      break;
+    case 2:
+      nameOfDay = "Wtorek";
+      break;
+    case 3:
+      nameOfDay = "Środa";
+      break;
+    case 4:
+      nameOfDay = "Czwartek"
+      break;
+    case 5:
+      nameOfDay = "Piątek";
+      break;
+    case 6:
+      nameOfDay = "Sobota";
+      break;
   }
-  function checkTime(i) {
-    if (i < 10) { i = "0" + i };
-    return i;
-  }
+  document.getElementById('clock').innerHTML =
+    `Dzisiaj jest ${nameOfDay} ${day}.${month}.${year}, godzina ${hour}: ${minute}:${second}.`;
+  let t = setTimeout(startTime, 500);
+}
+const checkTime = (i) => {
+  if (i < 10) { i = "0" + i };
+  return i;
+}
